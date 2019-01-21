a9beeaaf74814a12f389386a7c8458e29737c8a4
jiangyupeng <jiangyupeng@bjsasc.com>
2019-01-21 14:29:57 +0800

    tensorflow learn

---
+++ src/linearRegression/regression.py
@@ -0,0 +1,17 @@
+#-*- coding:utf-8 -*-
+
+"""
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+x_train = np.linspace(-1,1,101)
+print(x_train)
+y_train = 2*x_train + np.random.rand(*x_train.shape)*0.33
+print(x_train.shape)
+print(y_train)
+print(*x_train.shape)
+print(x_train.shape)
+m = [1,2]
+print(type(m))
+plt.scatter(x_train,y_train)
+plt.show()
