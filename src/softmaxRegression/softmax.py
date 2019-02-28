a385e68b0534de7c950dc663bb675bfd3db87245
jiangyupeng <jiangyupeng@bjsasc.com>
2019-02-28 09:12:40 +0800

    softmax

--- src/softmaxRegression/softmax.py
+++ src/softmaxRegression/softmax.py
@@ -10,6 +10,7 @@ import numpy as np
 import matplotlib
 matplotlib.use('TkAgg')
 from matplotlib import pyplot as plt
+import tensorflow as tf
 
 #B
 x1_label0 = np.random.normal(1,1,(100,1))
@@ -26,6 +27,80 @@ plt.scatter(x1_label1,x2_label1,c='g',marker='x',s=60)
 plt.scatter(x1_label2,x2_label2,c='b',marker='_',s=60)
 
 plt.show()
+
+xs_label0 = np.hstack((x1_label0,x2_label0))
+xs_label1 = np.hstack((x1_label1,x2_label1))
+xs_label2 = np.hstack((x2_label1,x2_label2))
+xs = np.vstack((xs_label0,xs_label1,xs_label2))
+
+
+labels = np.matrix([[1.,0.,0.]]*len(x1_label0)+[[0.,1.,0.]]*len(x1_label1)+[[0.,0.,1.]]*len(x1_label2))
+
+arr = np.arange(xs.shape[0])
+np.random.shuffle(arr)
+print (arr)
+
+xs = xs[arr,:]
+
+label = labels[arr,:]
+
+test_x1_label0 = np.random.normal(1,1,(10,1))
+test_x2_label0 = np.random.normal(1,1,(10,1))
+test_x1_label1 = np.random.normal(5,1,(10,1))
+test_x2_label1 = np.random.normal(4,1,(10,1))
+test_x1_label2 = np.random.normal(8,1,(10,1))
+test_x2_label2 = np.random.normal(0,1,(10,1))
+
+test_xs_label0 = np.hstack((test_x1_label0,test_x2_label0))
+test_xs_label1 = np.hstack((test_x1_label1,test_x2_label1))
+test_xs_label2 = np.hstack((test_x1_label2,test_x2_label2))
+test_labels = np.matrix([[1.,0.,0.]]*10 + [[0.,1.,0.]]*10+[[0.,0.,1.]]*10)
+
+print(test_xs_label0)
+print(test_labels)
+train_size,num_features = xs.shape
+
+
+'''
+@part: 4.10
+@title: Using softmax regression
+'''
+learning_rate = 0.01
+train_epochs = 1000
+num_labels = 3
+batch_size = 100
+
+X = tf.placeholder(tf.float32,shape=[None,num_features])
+Y = tf.placeholder(tf.float32,shape=[None,num_labels])
+
+W = tf.Variable(tf.zeros([num_features,num_labels]))
+b = tf.Variable(tf.zeros([num_labels]))
+y_model = tf.nn.softmax(tf.matmul(X,W) + b)
+
+cost = -tf.reduce_sum(Y * tf.log(y_model))
+train_op = tf.train.GradientDescentOptimizer(learning_rate).minimize(cost)
+
+correct_prediction = tf.equal(tf.argmax(y_model,1),tf.argmax(Y,1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction,tf.float32))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
