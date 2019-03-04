72d986ae36f017cd3aba15436e932884889270b8
jyp <jyp@192.168.0.17>
2019-03-05 07:47:49 +0800

    minist data process

---
+++ src/minist/TensorflowGoogleminist5/MNISTDataProcess5_1.py
@@ -0,0 +1,18 @@
+#codeing:utf-8
+'''
+Created on Mar 5, 2019
+
+@author: jyp
+'''
+#from tensorflow.examples.tutorials import minist
+import input_data
+mnist = input_data.read_data_sets("../../minist/",one_hot=True)
+#查看训练数据大小
+print(mnist.train.images.shape)
+print(mnist.train.labels.shape)
+#查看验证数据大小
+print(mnist.validation.images.shape)
+print(mnist.validation.labels.shape)
+#查看测试数据大小
+print(mnist.test.images.shape)
+print(mnist.test.labels.shape)
