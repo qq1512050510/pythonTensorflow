e161fe6774672987dabe52304011996c0b7b8de3
jyp <jyp@jypdembp>
2019-03-05 22:46:04 +0800

    neural network

--- src/minist/TensorflowGoogleminist5/MNISTDataProcess5_1.py
+++ src/minist/TensorflowGoogleminist5/MNISTDataProcess5_1.py
@@ -4,8 +4,8 @@ Created on Mar 5, 2019
 
 @author: jyp
 '''
-#from tensorflow.examples.tutorials import minist
-import input_data
+from tensorflow.examples.tutorials.mnist import input_data
+#import input_data
 mnist = input_data.read_data_sets("../../minist/",one_hot=True)
 #查看训练数据大小
 print(mnist.train.images.shape)
---
+++ src/minist/TensorflowGoogleminist5/NeuralNetwork.py
@@ -0,0 +1,10 @@
+#codeing:utf-8
+'''
+Created on Mar 5, 2019
+
+@author: jyp
+'''
+import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import input_data
+
+mnist = input_data.read_data_sets("../../minist/",one_hot=True)
