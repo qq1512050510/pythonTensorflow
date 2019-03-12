60259da83ca50d7f32394443c8084cc120ef71e3
jiangyupeng <jiangyupeng@bjsasc.com>
2019-03-12 17:31:04 +0800

    neural network

--- src/minist/TensorflowGooglemnist5/NeuralNetwork5_5_mnist_train.py
+++ src/minist/TensorflowGooglemnist5/NeuralNetwork5_5_mnist_train.py
@@ -7,7 +7,6 @@ Created on 2019年3月12日
 import os
 import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data
-from minist.TensorflowGooglemnist5 import NeuralNetwork5_5_mnist_inference
 
 #加载mnist_inference.py中定义的变量和前向传播的函数
 import NeuralNetwork5_5_mnist_inference
@@ -30,5 +29,17 @@ def train(mnist):
     y_ = tf.placeholder(tf.float32,[None,NeuralNetwork5_5_mnist_inference.OUTPUT_NODE],name="y-input")
     
     regularizer = tf.contrib.layer.l2_regularizer(REGULARZATION_RATE)
+    
+    #直接使用mnist_inference.py中定义的前向传播过程
+    y = NeuralNetwork5_5_mnist_inference.inference(x,regularizer)
+    global_step = tf.Variable(0,trainable=False)
+    
+    #和5.2.1小节样例中类似地定义损失函数、学习率、滑动平均操作以及训练过程。
+    variable_averages = tf.train.ExponentialMovingAverage(MOVING_AVERAGE_DECAY,global_step)
+    variable_averages_op = variable_averages.apply(tf.trainable_variables())
+    
+    print(variable_averages_op)
+    cross_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits(y,tf.argmax(y_,1))
+    cross_entropy_mean = tf.reduce_mean(cross_entropy)
 
 
