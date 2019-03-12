40ae7afb60c05ce000d0ec941a33aeed9d58b99c
jyp <jyp@192.168.0.17>
2019-03-12 21:14:05 +0800

    Tensorflow 5.5

--- src/minist/TensorflowGooglemnist5/NeuralNetwork5_5_mnist_train.py
+++ src/minist/TensorflowGooglemnist5/NeuralNetwork5_5_mnist_train.py
@@ -11,24 +11,28 @@ from tensorflow.examples.tutorials.mnist import input_data
 #加载mnist_inference.py中定义的变量和前向传播的函数
 import NeuralNetwork5_5_mnist_inference
 
+# compatible mac system
+os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
+
+
 #配置神经网络的参数
 BATCH_SIZE = 100
 LEARNING_RATE_BASE = 0.8
-LEARNING_RAGE_DECAY = 0.99
+LEARNING_RATE_DECAY = 0.99
 REGULARZATION_RATE = 0.0001
 TRAINING_STEPS = 30000
 MOVING_AVERAGE_DECAY = 0.99
 
 #模型保存的路径和文件名
 MODEL_SAVE_PATH = "./model/"
-MODEL_NAME = "model.ckpt"
+MODEL_NAME = "model5part.ckpt"
 
 def train(mnist):
     #定义输入输出placeholder
     x = tf.placeholder(tf.float32,[None,NeuralNetwork5_5_mnist_inference.INPUT_NODE],name="x-input")
     y_ = tf.placeholder(tf.float32,[None,NeuralNetwork5_5_mnist_inference.OUTPUT_NODE],name="y-input")
     
-    regularizer = tf.contrib.layer.l2_regularizer(REGULARZATION_RATE)
+    regularizer = tf.contrib.layers.l2_regularizer(REGULARZATION_RATE)
     
     #直接使用mnist_inference.py中定义的前向传播过程
     y = NeuralNetwork5_5_mnist_inference.inference(x,regularizer)
@@ -36,10 +40,50 @@ def train(mnist):
     
     #和5.2.1小节样例中类似地定义损失函数、学习率、滑动平均操作以及训练过程。
     variable_averages = tf.train.ExponentialMovingAverage(MOVING_AVERAGE_DECAY,global_step)
-    variable_averages_op = variable_averages.apply(tf.trainable_variables())
+    variables_averages_op = variable_averages.apply(tf.trainable_variables())
     
-    print(variable_averages_op)
-    cross_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits(y,tf.argmax(y_,1))
+    print(variables_averages_op)
+    #cross_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits(y,tf.argmax(y_,1))
+    cross_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=y,labels=tf.argmax(y_,1))
     cross_entropy_mean = tf.reduce_mean(cross_entropy)
+    #tf.add_n()列表中的元素相加
+    loss = cross_entropy_mean + tf.add_n(tf.get_collection('losses'))
+    learning_rate = tf.train.exponential_decay(LEARNING_RATE_BASE,global_step,mnist.train.num_examples/BATCH_SIZE,LEARNING_RATE_DECAY)
+    train_step = tf.train.GradientDescentOptimizer(learning_rate).minimize(loss,global_step=global_step)
+    with tf.control_dependencies([train_step,variables_averages_op]):
+        train_op = tf.no_op(name='train')
+    
+    #初始化TensorFlow持久化类。
+    saver = tf.train.Saver()
+    with tf.Session() as sess:
+        tf.global_variables_initializer().run()
+        '''
+        在训练过程中不再测试模型在验证数据上的表现，验证和测试的过程会有一个
+        独立的程序来完成。
+        '''
+        for i in range(TRAINING_STEPS):
+            xs,ys = mnist.train.next_batch(BATCH_SIZE)
+            _,loss_value,step = sess.run([train_op,loss,global_step],feed_dict={x:xs,y_:ys})
+            
+            #每1000轮保存一次模型。
+            if i%1000 == 0:
+                '''
+                输出当前的训练情况。这里只输出了当前训练batch上的损失函数大小
+                通过损失函数的大小可以大概了解训练的情况。在验证数据集上的准确率会
+                有一个单独的程序来完成。
+                '''
+                print("After %d training step(s),loss on training batch is %g"%(step,loss_value))
+                '''
+                保存当前的模型。注意这里给出了global_step参数，这样可以让每个被保存模型的文件末尾加上训练的轮数，
+                比如，“modal.ckpt=1000”表示训练1000轮之后得到的模型。
+                '''
+                saver.save(sess,os.path.join(MODEL_SAVE_PATH,MODEL_NAME),global_step=global_step)
+
+def main(argv=None):
+    mnist = input_data.read_data_sets('../../mnist',one_hot=True)
+    train(mnist)        
+
+if __name__=='__main__':
+    tf.app.run()
 
 
